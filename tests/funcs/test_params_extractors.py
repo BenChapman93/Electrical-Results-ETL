f2159fbe2fbf76d749ffbb932c77ff3169e6cffc
Ben Chapman <ben.chapman@p2i.com>
2022-01-12 20:43:38 +0000

    Added remaining unit tests for sample_number_extractor function

--- tests/funcs/test_params_extractors.py
+++ tests/funcs/test_params_extractors.py
@@ -12,3 +12,33 @@ class TestSampleNumberExtractor(object):
 
         assert actual == expected, message
 
+    def test_with_clean_tapwater_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 13 MIN TAPBJC.txt"
+        
+        expected = 20
+        actual = sample_number_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_empty_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\empty_file.txt"
+        
+        expected = None
+        actual = sample_number_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_random_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\random_text.txt"
+        
+        expected = None
+        actual = sample_number_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
