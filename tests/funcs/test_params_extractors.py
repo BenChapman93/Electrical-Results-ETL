07c2b483861b7296161c18c35189eb59b0e6916b
Ben Chapman <benchapman93@live.com>
2022-01-12 14:49:56 +0000

    Adding inital tests for params_extractors.py. Added __pycache__.py to .gitignore

---
+++ tests/funcs/test_params_extractors.py
@@ -0,0 +1,14 @@
+from src.funcs.params_extractors import sample_number_extractor, voltage_extractor, duration_min_extractor, user_extractor, get_mod_date_of_file
+
+class TestSampleNumberExtractor(object):
+
+    def test_with_clean_salt_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 1 HRS SALTBJC.txt"
+        
+        expected = 20
+        actual = sample_number_extractor(file)
+        message = f"Expected: 20, Actual: {actual}"
+
+        assert actual == expected, message
+
