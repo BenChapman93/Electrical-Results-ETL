ce2d791f131396ff912d965e9afc34e3d13d1d95
Ben Chapman <benchapman93@live.com>
2022-01-13 09:18:21 +0000

    Added all remaining unit tests for params_extractors.py. Ready to merge with master.

--- tests/funcs/test_params_extractors.py
+++ tests/funcs/test_params_extractors.py
@@ -42,3 +42,172 @@ class TestSampleNumberExtractor(object):
 
         assert actual == expected, message
 
+class TestVoltageExtractor(object):
+
+    def test_with_clean_salt_file(self):
+
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 1 HRS SALTBJC.txt"
+        
+        expected = 4.3
+        actual = voltage_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_tapwater_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 13 MIN TAPBJC.txt"
+        
+        expected = 8.0
+        actual = voltage_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_empty_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\empty_file.txt"
+        
+        expected = None
+        actual = voltage_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_random_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\random_text.txt"
+        
+        expected = None
+        actual = voltage_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+class TestDurationMinExtractor(object):
+
+    def test_with_clean_salt_file(self):
+
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 1 HRS SALTBJC.txt"
+        
+        expected = 60
+        actual = duration_min_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_tapwater_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 13 MIN TAPBJC.txt"
+        
+        expected = 13
+        actual = duration_min_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_empty_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\empty_file.txt"
+        
+        expected = None
+        actual = duration_min_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_random_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\random_text.txt"
+        
+        expected = None
+        actual = duration_min_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+class TestUserExtractor(object):
+
+    def test_with_clean_salt_file(self):
+
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 1 HRS SALTBJC.txt"
+        
+        expected = 'BJC'
+        actual = user_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_tapwater_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 13 MIN TAPBJC.txt"
+        
+        expected = 'BJC'
+        actual = user_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_empty_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\empty_file.txt"
+        
+        expected = None
+        actual = user_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_random_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\random_text.txt"
+        
+        expected = None
+        actual = user_extractor(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+class TestGetModDateOfFile(object):
+
+    def test_with_clean_salt_file(self):
+
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 1 HRS SALTBJC.txt"
+        
+        expected = '28-04-2021'
+        actual = get_mod_date_of_file(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_tapwater_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\Complete 13 MIN TAPBJC.txt"
+        
+        expected = '28-04-2021'
+        actual = get_mod_date_of_file(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_empty_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\empty_file.txt"
+        
+        expected = '10-01-2022'
+        actual = get_mod_date_of_file(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+    def test_with_clean_random_file(self):
+        
+        file = r"C:\Users\ben.chapman\Desktop\Udemy\Electrical_Results_ETL\mock_files\random_text.txt"
+        
+        expected = '12-01-2022'
+        actual = get_mod_date_of_file(file)
+        message = f"Expected: {expected}, Actual: {actual}"
+
+        assert actual == expected, message
+
+
