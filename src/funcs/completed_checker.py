664d57321eb55b241db84d4b43de9ce2e93e31c9
Ben Chapman <benchapman93@live.com>
2022-01-14 10:56:24 +0000

    Added TempFile class to completed_checker.py

--- src/funcs/completed_checker.py
+++ src/funcs/completed_checker.py
@@ -2,6 +2,18 @@ from src.funcs.params_extractors import duration_min_extractor, sample_number_ex
 import os
 from shutil import copy2
 
+class TempFile():
+    
+    def __init__(self, path):
+        self.path = path
+        self.temp_file_path = temp_file_path_generator(path)
+        
+    def __enter__(self):
+        return copy2(self.path, self.temp_file_path)
+    
+    def __exit__(self, types, value, traceback):
+        os.remove(self.temp_file_path)
+
 def temp_file_path_generator(file_path):
     """Function to generate a temporary file path for any .txt file passed.
 
