d5e36a7afae6e967d84bab6678e9522d07498e30
Ben Chapman <ben.chapman@p2i.com>
2022-01-15 19:59:52 +0000

    Added completed_checker function. Branch ready for merge.

--- src/funcs/completed_checker.py
+++ src/funcs/completed_checker.py
@@ -29,4 +29,25 @@ def temp_file_path_generator(file_path):
     
     return temp_file_path
 
+def completed_checker(file):
 
+    duration = duration_min_extractor(file)
+    sample_number = sample_number_extractor(file)
+
+    if duration != None and sample_number != None:
+        
+        with TempFile(file) as temp_file:
+            with open(temp_file, "r") as f:
+
+                lines = f.readlines()
+                last_line = lines[-1].split()
+                timestamp = int(last_line[0]) / 60
+                completed_channels = len(last_line) - 1
+
+                if duration == timestamp and sample_number == completed_channels:
+                    return file
+                else:
+                    return None
+        
+    else:
+        return None
